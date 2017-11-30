f2cb8c12b7757bc924294f8ecabb411e8821412b
front-end-bull <2531085993@qq.com>
2017-11-30 13:37:26 +0800

    over

---
+++ advertisements/hrefList.js
@@ -0,0 +1,102 @@
+var hrefList = [
+    {
+        type:'A 少儿保险',
+        items:[
+            {
+                name:'我们和50位妈妈沟通后，推荐这样为宝宝买保险',
+                href:'http://www.baoxianshenqi.cn/geihaizimaibaoxian.html'
+            },
+            {
+                name:'额度高、保障全面，少儿保险这款很不错',
+                href:'http://www.baoxianshenqi.cn/shaoeryiwaixian.html'
+            },
+            {
+                name:'为什么万能险+重疾害人不浅？',
+                href:'http://www.baoxianshenqi.cn/maicuobaoxian.html'
+            },
+            {
+                name:'住院医疗险、门诊险横向测评',
+                href:'http://www.baoxianshenqi.cn/baoxiaozifeiyao.html'
+            },
+            {
+                name:'“教育金”保得住你儿女的教育储备吗？',
+                href:'http://www.baoxianshenqi.cn/jiaoyujin.html'
+            },
+
+        ]
+    },
+    {
+        type:'B 成人保险',
+        items:[
+            {
+                name:'终身型重疾险，这款性价比最高',
+                href:'http://www.baoxianshenqi.cn/zhongjixianduibi.html'
+            },
+            {
+                name:'百万医疗险横向对比分析',
+                href:'http://www.baoxianshenqi.cn/yiliaoxianceping.html'
+            },
+            // {
+            //     name:'2017消费型重疾险横向测评',
+            //     href:'http://www.baoxianshenqi.cn/yiliaoxianceping.html'
+            // },
+            {
+                name:'老年人保险最全购买指南',
+                href:'http://www.baoxianshenqi.cn/geibamamaibaoxian.html'
+            },
+            {
+                name:'成人意外险性价比之王，点这里',
+                href:'http://www.baoxianshenqi.cn/yiwaixian.html'
+            },
+            {
+                name:'收入不高家庭，购买保险清单',
+                href:'http://www.baoxianshenqi.cn/dishouru.html'
+            },
+            {
+                name:'交了这么久，我们到底可以领回多少养老金？',
+                href:'http://www.baoxianshenqi.cn/yanglaojin.html'
+            },
+            {
+                name:'精算师说分红险，是“鸡肋”还是“刚需”',
+                href:'http://www.baoxianshenqi.cn/fenhongxian.html'
+            },
+        ]
+    },
+    {
+        type:'C 投保技巧',
+        items:[
+            {
+                name:'不同年龄段，如何购买保险？',
+                href:'http://www.baoxianshenqi.cn/how2plan.html'
+            },
+            {
+                name:'科学投保5大原则，买保险前必看',
+                href:'http://www.baoxianshenqi.cn/how2choose.html'
+            },
+            {
+                name:'医保的全面说明，看这里',
+                href:'http://www.baoxianshenqi.cn/yibaoquanmianshuoming.html'
+            },
+            {
+                name:'买错保险想退保，专家这么说',
+                href:'http://www.baoxianshenqi.cn/tuibaozhinan.html'
+            },
+            {
+                name:'买保险，要如何进行健康告知？',
+                href:'http://www.baoxianshenqi.cn/shicaozhinan.html'
+            },
+        ]
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
